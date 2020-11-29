5e554f70ece71401968249bf115e44f2e9391509
AntonJanto <anton.janto@gmail.com>
2020-11-29 13:10:18 +0100

    Salting hashing and verification on middleware

--- FamilyAPI/Middleware/Authentication/SqliteUserService.cs
+++ FamilyAPI/Middleware/Authentication/SqliteUserService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using FamilyAPI.DataAccess;
 using FamilyAPI.Models.Authentication;
@@ -17,11 +18,35 @@ namespace FamilyAPI.Middleware.Authentication
 
         public async Task<User> ValidateUserAsync(string usermame, string password)
         {
-            var user = await _familyApiContext.Users.FindAsync(usermame);
-            
-            return user?.Password == password ? 
-                user : 
-                throw new Exception("Incorrect username or password");
+            var savedUser = await _familyApiContext.Users.FindAsync(usermame);
+
+            if (savedUser == null) throw new Exception("Incorrect username or password");
+            var isValidUser = ValidatePassword(password, savedUser.Password);
+            if (isValidUser)
+            {
+                return savedUser;
+            }
+            throw new Exception("Incorrect username or password");
+        }
+
+        private static bool ValidatePassword(string password, string savedPw)
+        {
+            byte[] hashBytes = Convert.FromBase64String(savedPw);
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 15000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
         }
 
         public async Task RegisterUserAsync(string username, string password)
@@ -34,11 +59,27 @@ namespace FamilyAPI.Middleware.Authentication
                 var newUser = new User
                 {
                     Username = username,
-                    Password = password
+                    Password = HashAndSalt(password)
                 };
                 await _familyApiContext.Users.AddAsync(newUser);
                 await _familyApiContext.SaveChangesAsync();
             }
+        }
+
+        private string HashAndSalt(string pw)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(pw, salt, 15000);
+            var hash = pbkdf2.GetBytes(20);
+
+            var hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPwHash = Convert.ToBase64String(hashBytes);
+            return savedPwHash;
         }
     }
 }
